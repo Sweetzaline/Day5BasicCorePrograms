d22fb8b888f0fe8d105cb9a3edf515081bfab2a7
unknown <buragohain4@gmail.com>
2022-07-31 04:25:05 +0530

    Checking answer for various power of two

---
+++ PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the exponential number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Program.PoweroTwo(y);
+        }
+        public static void PoweroTwo(int num)
+        {
+
+            int temp = num;
+            int answer = 1;
+            while(num != 0)
+            {
+                answer = answer * 2;
+                num = num - 1;
+            }
+            Console.WriteLine($"Power of 2 ^ {temp} is: {answer}");
+           
+        }
+    }
+}
