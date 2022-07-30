b1c651ba787781825cb35c7549af8b02d3983375
unknown <buragohain4@gmail.com>
2022-07-31 04:04:12 +0530

    Checking Leap Year

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public class LY
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            LY.LeapYear(y);
+
+        }
+        public static void LeapYear(int year)
+        {
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine($"{year} is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a Leap Year");
+            }
+        }       
+    }
+}
