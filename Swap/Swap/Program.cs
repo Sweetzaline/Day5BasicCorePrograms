2d887fbd7323e11a9534c341273cec1c4172df23
unknown <buragohain4@gmail.com>
2022-07-31 05:23:19 +0530

    Swapping Numbers

---
+++ Swap/Swap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"First No.= {a} and Second No.= {b} ");
+        }
+    }
+}
