464e132e4a7c836262c74bfea487e93005a2332d
unknown <buragohain4@gmail.com>
2022-07-31 04:38:33 +0530

    Calculating Harmonic number

---
+++ Harmonic/Harmonic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Harmonic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Program.HarmonicNo(y);
+        }
+
+        public static void HarmonicNo(int num)
+        {
+            double answer = 0;
+            for(int i =1; i<=num; i++)
+            {
+                answer = (answer + 1) / i;
+            }
+            Console.WriteLine($"Harmonic No. = {answer}");
+        }
+    }
+}
