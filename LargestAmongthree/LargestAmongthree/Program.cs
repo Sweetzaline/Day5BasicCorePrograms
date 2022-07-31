cb0fac480b1f4e10a3d96c8b89b00f1d44dbdfb7
unknown <buragohain4@gmail.com>
2022-07-31 06:08:23 +0530

    Finding Largest amongst three numbers

---
+++ LargestAmongthree/LargestAmongthree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LargestAmongthree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            
+            int largest = num1;
+            int[] a = { num1, num2, num3 };
+            foreach (var b in a)
+            {
+                if (b > largest)
+                {
+                    largest = b;
+                }
+            }
+           
+           Console.WriteLine("Largest Number is " +largest);
+        }
+    }
+}
