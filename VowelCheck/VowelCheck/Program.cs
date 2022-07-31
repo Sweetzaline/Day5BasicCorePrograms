5ae9733767c08e6f6fce44fd58768e6f06cf5978
unknown <buragohain4@gmail.com>
2022-07-31 05:53:01 +0530

    Checking Vowel or Consonant

---
+++ VowelCheck/VowelCheck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VowelCheck
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Alphabet: ");
+            char c;
+            c = Console.ReadKey().KeyChar;
+
+            if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
+            {
+                Console.WriteLine(", is Vowel");
+            }
+            else
+            {
+                Console.WriteLine(",  is a Consonant");
+            }
+        }
+    }
+}
